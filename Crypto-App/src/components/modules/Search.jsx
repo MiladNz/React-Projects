83f409635ff3f4aaa1e4e64567a9a84d52c0fc11
miladn <milad.n91@gmail.com>
2024-09-25 11:20:38 +0330

    add cleanup function

--- Crypto-App/src/components/modules/Search.jsx
+++ Crypto-App/src/components/modules/Search.jsx
@@ -7,14 +7,28 @@ function Search({ currency, setCurrency }) {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
+    const controller = new AbortController();
     if (!text) return;
 
     const search = async () => {
-      const res = await fetch(searchCoin(text));
-      const json = await res.json();
-      if (json.coins) setCoins(json);
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        if (json.coins) {
+          setCoins(json.coins);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          alert(error.message);
+        }
+      }
     };
     search();
+    return () => controller.abort();
   }, [text]);
 
   return (
