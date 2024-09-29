8d4b369c7ccaa179bfc883905042242d83b4cf00
miladn <milad.n91@gmail.com>
2024-09-29 21:01:16 +0330

    Add typeHandler

--- Crypto-App/src/services/cryptoApi.js
+++ Crypto-App/src/services/cryptoApi.js
@@ -8,6 +8,6 @@ const searchCoin = (query) =>
   `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
 
 const marketChart = (coin) =>
-  `${BASE_URL}/coins/${coin}/market_chart?vs_currency=usd&days=7`;
+  `${BASE_URL}/coins/${coin}/market_chart?vs_currency=usd&days=7&x_cg_demo_api_key=${API_KEY}`;
 
 export { getCoinList, searchCoin, marketChart };
